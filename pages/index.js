d335b3883a8316deb5c690738acc5090fd42a2fe
Ongthepmakemnhungnong <pazzzz123456@gmail.com>
2024-05-31 10:24:07 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,26 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Sắt Thép Biên Hòa" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+<h1><strong>Sắt Thép Biên Hòa - Công Ty Lộc Thiên</strong></h1>
+<div><a href="https://satthepbienhoa.vn/">Sắt Thép Biên Hòa</a> is one of the leading companies in the South, specializing in providing a variety of high quality Hoa Phat Steel Pipe and Hoa Phat steel square tube products. With many years of experience in the industry, we are committed to providing customers with prestige and outstanding quality.</div>
+<div></div>
+<div>A strong point of Sat Thep Bien Hoa - Loc Thien Company is its competitive prices. We always strive to offer reasonable and market-appropriate prices, bringing customer satisfaction. At the same time, we not only focus on providing high-value products, but also always commit to dedicated and thoughtful service.</div>
+<div></div>
+<div>With a team of experienced staff and deep understanding of the iron and steel industry, Sat Thep Bien Hoa - Loc Thien Company confidently brings customers the best quality products. We are not only a reliable supplier, but also a reliable partner in the construction and development of projects.</div>
+<div></div>
+<div>With Sat Thep Bien Hoa, customers can be assured of the quality of iron and steel products and professional services. We commit to always be willing to support and accompany our customers to bring success and progress in every construction project.</div>
+<div></div>
+<div>Contact Info:</div>
+<div></div>
+<div>Website: <a href="https://satthepbienhoa.vn/">satthepbienhoa.vn</a></div>
+<div></div>
+<div>Google Maps tại &lt;a href="https://maps.app.goo.gl/fv8YowoNpnqDmnmQ8 "&gt;Google Map&lt;/a&gt;</div>
+<div></div>
+<div>Fanpage &lt;a href="https://www.facebook.com/congtylocthien"&gt;facebook&lt;/a&gt;</div>
+<div></div>
+<div>Truy cập website google:&lt;a href="https://sites.google.com/view/satthepbienhoa/trang-chủ"&gt;Google Site&lt;/a&gt;</div>
         </p>
       </main>
 
