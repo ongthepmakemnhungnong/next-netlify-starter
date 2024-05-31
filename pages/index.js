e9e5597e0e534645ce6f65a2a3ea437756f4bc4a
Ongthepmakemnhungnong <pazzzz123456@gmail.com>
2024-05-31 10:46:38 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,40 +6,82 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <section class="content4 cid-scbObQMNYs" id="content4-o">
+  <div class="container-fluid">
+      <div class="row justify-content-center">
+          <div class="title col-md-12 col-lg-12">
+              <h3 class="mbr-section-title mbr-fonts-style align-center mb-4 display-2"><strong>View Lưới Sắt B40 PNG</strong></h3>
+          </div>
+          <div class="mb-1"> <center><!-- fixed_size_300 --></center> </div>
+      </div>
+  </div>
+</section>
+<section class="content5 cid-scgf3IiveW" id="content5-q">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 col-lg-10">
+                <div class="mbr-text mbr-fonts-style display-7">
+                    
+<article>
+    <p><strong>View Lưới Sắt B40 PNG</strong>. Bảng báo giá lưới rào b40 tại công ty sài gòn cmc. Cứ như vậy bạn có thể tham khảo trọng lượng của các khổ lưới khác.</p>
 
-      <main>
-        <Header title="Sắt Thép Biên Hòa" />
-        <p className="description">
-<h1><strong>Sắt Thép Biên Hòa - Công Ty Lộc Thiên</strong></h1>
-<div><a href="https://satthepbienhoa.vn/">Sắt Thép Biên Hòa</a> is one of the leading companies in the South, specializing in providing a variety of high quality Hoa Phat Steel Pipe and Hoa Phat steel square tube products. With many years of experience in the industry, we are committed to providing customers with prestige and outstanding quality.</div>
-<div></div>
-<div>A strong point of Sat Thep Bien Hoa - Loc Thien Company is its competitive prices. We always strive to offer reasonable and market-appropriate prices, bringing customer satisfaction. At the same time, we not only focus on providing high-value products, but also always commit to dedicated and thoughtful service.</div>
-<div></div>
-<div>With a team of experienced staff and deep understanding of the iron and steel industry, Sat Thep Bien Hoa - Loc Thien Company confidently brings customers the best quality products. We are not only a reliable supplier, but also a reliable partner in the construction and development of projects.</div>
-<div></div>
-<div>With Sat Thep Bien Hoa, customers can be assured of the quality of iron and steel products and professional services. We commit to always be willing to support and accompany our customers to bring success and progress in every construction project.</div>
-<div></div>
-<div>
+    <p>
+                        <figure>
+            <img src="http://hoabinhsteel.com/wp-content/uploads/2019/04/L%C6%AF%E1%BB%9AI-K%E1%BA%BCM-1-2.jpg" alt="LÆ°á»i B40" style="width: 100%; padding: 5px; background-color: grey;"  onerror="this.onerror=null;this.src='https://lh3.googleusercontent.com/proxy/e1axlk9AtfRhd9Muwz5_dmJ2Zwk4LjjoLJ_5P2YP8OVQtnBqy459eaHfOgnRvPzZCEUmK0ahbcFOKFAUFMe0dE0V-bkiMP-O1PsRAqkTKDb5k3CcWuVznoKz_BUbHNQ9tw_GogAlhV05c1eOimujvw';">
+            <figcaption>LÆ°á»i B40 from hoabinhsteel.com</figcaption>
+        </figure>
+                Vui lòng chọn mua thêm trong shop khách sỉ liên hệ 0948621805 để được báo giá tốt nhất #decor #khungtranh #khungluoithep #khungluoitheptrangtri. Một hàng đá hộc bọc lưới sắt b40 dài khoảng 200 m đâm ngang bãi tắm d biển sầm sơn (thanh hóa) không những làm biến dạng bãi biển mà còn vô tình tạo nên những cái rác, túi bóng nilon đủ các loại mắc vào lưới sắt b40 trông mất mĩ quan bãi tắm được xem là thuộc nhóm đẹp nhất ở miền bắc. Lưới b40 1m2, độ dày 3 ly có trọng lượng là 1,8 kg (nền màu vàng trên hình).
+    </p>
 
-[caption id="attachment_10912" align="aligncenter" width="1000"]<img class="size-full wp-image-10912" src="https://satthepbienhoa.vn/wp-content/uploads/2024/04/Bang-gia-thep-hop-Hoa-Phat-tai-Binh-Duong.jpg" alt="Bảng giá thép hộp Hòa Phát tại Bình Dương" width="1000" height="750" /> Bảng giá thép hộp Hòa Phát tại Bình Dương[/caption]
+    <h3>Các tin tức liên quan.</h3>
+    <p>Công thức lưỡi bò nướng này ta sẽ học tập của người nhật nhé! Nhà phân phối cấp 1 các loại sắt thép xây dựng, thép ống, thép hộp, tôn, xà gồ. Một hàng đá hộc bọc lưới sắt b40 dài khoảng 200 m đâm ngang bãi tắm d biển sầm sơn (thanh hóa) không những làm biến dạng bãi biển mà còn vô tình tạo nên những cái rác, túi bóng nilon đủ các loại mắc vào lưới sắt b40 trông mất mĩ quan bãi tắm được xem là thuộc nhóm đẹp nhất ở miền bắc. Sự phân bố mạng lưới đường sắt trên thế giới phản ánh khá rõ sự phân bố của ngành nào ở các nước ta và châu lục ?</p>
+</article>
 
-</div>
-<div></div>
-<div>Contact Info:</div>
-<div></div>
-<div>Website: <a href="https://satthepbienhoa.vn/">satthepbienhoa.vn</a></div>
-<div></div>
-<div>
-<div>Google Maps</div>
-<div></div>
-<div>Facebook fanpage</div>
-<div></div>
-<div>Visit the Google Sites website</div>
-</div>
-        </p>
+<section>
+    <aside>
+        <a href="https://satthepmanhphat.vn/wp-content/uploads/2018/09/trong-luong-luoi-b40-720x480.jpg" target="_blank"><img alt="Trá»ng LÆ°á»£ng LÆ°á»i B40 Co Máº¥y Loáº¡i Sáº¯t Thep Máº¡nh Phat" src="https://satthepmanhphat.vn/wp-content/uploads/2018/09/trong-luong-luoi-b40-720x480.jpg" width="100%" onerror="this.onerror=null;this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTI4gnFFQFt3dPvJWLEY77dU15c8IJ-a49_zw&amp;usqp=CAU';"></a>
+        <small>Source: satthepmanhphat.vn</small>
+        <p>Luôn có giá tốt khi gọi ngay đến hotline 0936.600.600.</p>
+    </aside>
+    <aside>
+        <a href="https://nguyenmuon.com/wp-content/uploads/2019/07/luoi-thep-b40-nguyen-muon.jpg" target="_blank"><img alt="LÆ°á»i Thep B40 Chinh Hang Nhiá»u Æ°u Äai Nguyen Muon" src="https://nguyenmuon.com/wp-content/uploads/2019/07/luoi-thep-b40-nguyen-muon.jpg" width="100%" onerror="this.onerror=null;this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSKc24MgjfrGH0z6Fs_ZkgH3KNLnulheYTYRg&amp;usqp=CAU';"></a>
+        <small>Source: nguyenmuon.com</small>
+        <p>Vui lòng chọn mua thêm trong shop khách sỉ liên hệ 0948621805 để được báo giá tốt nhất #decor #khungtranh #khungluoithep #khungluoitheptrangtri.</p>
+    </aside>
+    <aside>
+        <a href="https://www.satthepxaydungvn.com/uploads/category/luoi-b40.jpg" target="_blank"><img alt="Báº£ng Bao Gia LÆ°á»i B40 Quy Cach Khá» 1m2 Äáº¿n 2m5 Cáº­p Nháº¥t Má»i Nháº¥t" src="https://www.satthepxaydungvn.com/uploads/category/luoi-b40.jpg" width="100%" onerror="this.onerror=null;this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcROGjgLUsyqoingbNQFE6Hwz5cN-94kHBiu7w&amp;usqp=CAU';"></a>
+        <small>Source: www.satthepxaydungvn.com</small>
+        <p>Để có bảng giá lưới b40 chính xác nhất, quý khách vui lòng liên hệ hotline, gửi email hoặc chat trực tiếp để nhân viên chúng tôi hỗ trợ.</p>
+    </aside>
+    <aside>
+        <a href="https://satthepmanhphat.vn/wp-content/uploads/2018/09/1-cuon-luoi-b40-bao-nhieu-kg.jpg" target="_blank"><img alt="Trá»ng LÆ°á»£ng LÆ°á»i B40 Co Máº¥y Loáº¡i Sáº¯t Thep Máº¡nh Phat" src="https://satthepmanhphat.vn/wp-content/uploads/2018/09/1-cuon-luoi-b40-bao-nhieu-kg.jpg" width="100%" onerror="this.onerror=null;this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRtppHxBbONYudmqIIm9RkuN3CUn7kqumCB1w&amp;usqp=CAU';"></a>
+        <small>Source: satthepmanhphat.vn</small>
+        <p>Công ty sắt thép hoàng long chuyên cung cấp các mặt hàng sắt thép giá rẻ tại bình dương.</p>
+    </aside>
+    <aside>
+        <a href="https://satthepmanhphat.vn/wp-content/uploads/2018/08/bao-gia-luoi-b40-nam-2020-800x480.jpg" target="_blank"><img alt="Gia LÆ°á»i B40 2020 Má»i Nháº¥t LÆ°á»i Thep B40 Khá» 1m 2m 2 4m" src="https://satthepmanhphat.vn/wp-content/uploads/2018/08/bao-gia-luoi-b40-nam-2020-800x480.jpg" width="100%" onerror="this.onerror=null;this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSdKZsrz_2DIEvO5sdr9VhPF09H39edQLcA2A&amp;usqp=CAU';"></a>
+        <small>Source: satthepmanhphat.vn</small>
+        <p>Lưới b40 1 mét 2 bao nhiêu kg.</p>
+    </aside>
+    <aside>
+        <a href="https://satthepbinhminh.com/wp-content/uploads/2019/05/luoi-b40-6x7.jpg" target="_blank"><img alt="LÆ°á»i B40 Binh Minh ThÆ°Æ¡ng Hiá»u Sá» 1 Báº£ng Gia LÆ°á»i B40 2020" src="https://satthepbinhminh.com/wp-content/uploads/2019/05/luoi-b40-6x7.jpg" width="100%" onerror="this.onerror=null;this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSoAEx56QjWSW0LDEgVBFlThQgSnm86lYlYrg&amp;usqp=CAU';"></a>
+        <small>Source: satthepbinhminh.com</small>
+        <p>Báo giá lưới b40 mới nhất.</p>
+    </aside>
+    <aside>
+        <a href="https://cauthangviet.net/wp-content/uploads/2020/03/gpu1559353707.jpg" target="_blank"><img alt="Máº«u Hang Rao LÆ°á»i B40 Äáº¹p Gia Ráº» Táº¡i Ha Ná»i" src="https://cauthangviet.net/wp-content/uploads/2020/03/gpu1559353707.jpg" width="100%" onerror="this.onerror=null;this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ8rN0cEl3-4p-esk86Woj7TtLg_i9VZJIvrQ&amp;usqp=CAU';"></a>
+        <small>Source: cauthangviet.net</small>
+        <p>Dũng lưới chuyên cung cấp lưới mắt cáo chất lượng cao được xoắn 3 đến 5 chao từ nhiều chất liệu cao cấp, quy cách 0.5 đến 1.0m x 10 đến 40m phù hợp với mọi nhu cầu sử dụng.</p>
+    </aside>
+    <aside>
+        <a href="https://driver.gianhangvn.com/file/hang-rao-luoi-b40-pnc5-1355828f28384.jpg" target="_blank"><img alt="Hang Rao LÆ°á»i Thep Hang Rao LÆ°á»i Thep Han Hang Rao LÆ°á»i B40" src="https://driver.gianhangvn.com/file/hang-rao-luoi-b40-pnc5-1355828f28384.jpg" width="100%" onerror="this.onerror=null;this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT2-QzKYbItHzuHVkqNRnUBgiFgCbMvIKTs7w&amp;usqp=CAU';"></a>
+        <small>Source: driver.gianhangvn.com</small>
+        <p>1 khung sắt, 1 dây đèn led, 2 đinh móc đóng thông minh quý khách muốn mua thêm phụ kiện khác như dây dù, kẹp gỗ, dây đèn.</p>
+    </aside>
+    <aside>
+        <a href="https://dailythepxaydung.net/wp-content/uploads/2018/10/ban-luoi-b40-chat-luong-763x480.jpg" target="_blank"><img alt="Äá»a Chá» Ban LÆ°á»i B40 Ráº» Nháº¥t Thá» TrÆ°á»ng Thep Xay Dá»±ng" src="https://dailythepxaydung.net/wp-content/uploads/2018/10/ban-luoi-b40-chat-luong-763x480.jpg" width="100%" onerror="this.onerror=null;this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQQGZq0pjDV3TnF7Xe_V7Iqave-4b9ORT4JHA&amp;usqp=CAU';"></a>
+        <small>Source: dailythepxaydung.net</small>
+        <p>Bundesstraße 40, a german road.</p>
       </main>
 
       <Footer />
